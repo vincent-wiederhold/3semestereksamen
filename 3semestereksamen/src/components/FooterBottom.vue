da9686453729130247973f976019ed84646ebc5b
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-13 14:48:18 +0100

    footer

--- 3semestereksamen/src/components/FooterBottom.vue
+++ 3semestereksamen/src/components/FooterBottom.vue
@@ -3,9 +3,37 @@
 <template>
     <div>
         <li>
-            <p>Dina Vejling</p>
+            <h4>Åbningstider</h4>
+            <p>Mandag - torsdag: kl. 11.00 - 17.30</p>
+            <p>Fredag: kl. 11.00 - 18.00</p>
+            <p>Sødag: Lukket</p>
         </li>
+        <li>
+            <h4>Konkaktoplysinger</h4>
+            <p>Brandtspassage 30, 5000 Odense C</p>
+            <p>info@dinavejling.dk</p>
+            <p>+45 28 30 14 05</p>
+        </li>
+        <li>
+            <h4>Informationer</h4>
+            <p>Om Dina Vejling</p>
+            <p>Handelsbetingelser</p>
+            <p>Galleri Rummet</p>
+            <p>Arrengementer</p>
+            <p>Kunstnere</p>
+        </li>
+        <li>
+            <h4>Følg Dina Vejling</h4>
+            <p>Instagram</p>
+            <p>Facebook</p>
+        </li>
+        <p>Dinas logo</p>
+
+        <p>@ 2025 Dina Vejling - Dansk Kunsthåndværk</p>
+        <p>Drevet af Dina Vejling</p>
     </div>
+
 </template>
 <style>
+
 </style>
