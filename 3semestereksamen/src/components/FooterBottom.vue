80532e9dbdc92e239ea2f490aae0a6483b5305f5
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-14 11:26:17 +0100

    headdings

--- 3semestereksamen/src/components/FooterBottom.vue
+++ 3semestereksamen/src/components/FooterBottom.vue
@@ -1,42 +1,42 @@
 <script setup>
 </script>
 <template>
-  
-    <div class="footercontent">
-        <h4>Åbningstider</h4>
-        <ul>
-            <li>Mandag - torsdag: kl. 11.00 - 17.30</li>
-            <li>Fredag: kl. 11.00 - 18.00</li>
-            <li>Sødag: Lukket</li>
-        </ul>
-        <h4>Konkaktoplysinger</h4>
-        <ul>
-            <li><font-awesome-icon icon="fa-solid fa-location-dot" class="fa-lg" /> Brandtspassage 30, 5000 Odense C</li>
-            <li><font-awesome-icon icon="fa-solid fa-envelope" class="fa-lg" /> info@dinavejling.dk</li>
-            <li><font-awesome-icon icon="fa-solid fa-phone" class="fa-lg"/> +45 28 30 14 05</li>
-        </ul>
-        <h4>Informationer</h4>
-        <ul>
-            <li>Om Dina Vejling</li>
-            <li>Handelsbetingelser</li>
-            <li>Galleri Rummet</li>
-            <li>Arrengementer</li>
-            <li>Kunstnere</li>
-        </ul>
-    
-        <h4>Følg Dina Vejling</h4>
-        <div class="some">
-            <font-awesome-icon icon="fa-brands fa-instagram" class="fa-4x"/>
-            <font-awesome-icon icon="fa-brands fa-square-facebook" class="fa-4x"/>
-        </div>
-        <img src="../assets/dinavejling.svg" alt="" class="logo">
+    <footer>
+        <div class="footercontent">
+            <h4>Åbningstider</h4>
+            <ul>
+                <li>Mandag - torsdag: kl. 11.00 - 17.30</li>
+                <li>Fredag: kl. 11.00 - 18.00</li>
+                <li>Sødag: Lukket</li>
+            </ul>
+            <h4>Konkaktoplysinger</h4>
+            <ul>
+                <li><font-awesome-icon icon="fa-solid fa-location-dot" class="fa-lg" /> Brandtspassage 30, 5000 Odense C</li>
+                <li><font-awesome-icon icon="fa-solid fa-envelope" class="fa-lg" /> info@dinavejling.dk</li>
+                <li><font-awesome-icon icon="fa-solid fa-phone" class="fa-lg"/> +45 28 30 14 05</li>
+            </ul>
+            <h4>Informationer</h4>
+            <ul>
+                <li>Om Dina Vejling</li>
+                <li>Handelsbetingelser</li>
+                <li>Galleri Rummet</li>
+                <li>Arrengementer</li>
+                <li>Kunstnere</li>
+            </ul>
         
-    </div>
-    <div class="footerbanner">
-        <p>@ 2025 Dina Vejling - Dansk Kunsthåndværk</p>
-        <p>Drevet af Dina Vejling</p>
-    </div>
-
+            <h4>Følg Dina Vejling</h4>
+            <div class="some">
+                <font-awesome-icon icon="fa-brands fa-instagram" class="fa-4x"/>
+                <font-awesome-icon icon="fa-brands fa-square-facebook" class="fa-4x"/>
+            </div>
+            <img src="../assets/dinavejling.svg" alt="" class="logo">
+            
+        </div>
+        <div class="footerbanner">
+            <p>@ 2025 Dina Vejling - Dansk Kunsthåndværk</p>
+            <p>Drevet af Dina Vejling</p>
+        </div>
+    </footer>
 </template>
 
 <style>
@@ -48,17 +48,6 @@
     font-optical-sizing: auto;
     font-weight: 300;
     font-style: normal;
-}
-
-body{
-    background-color: #5B4A4B;
-    color: white;
-}
-
-h4{
-    font-family: 'Museo', sans-serif;
-    font-size: 20px;                                           
-    font-weight: bold;
 }
 
 .footercontent{
