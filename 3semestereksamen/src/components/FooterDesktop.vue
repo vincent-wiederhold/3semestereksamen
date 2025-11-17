64fa1c5db72d84f37ea437a5fb0b7275c6be5e47
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-17 09:14:06 +0100

    nyt view
    Ikke sikkert at der skal et nyt view til, jeg undersyger mulighederne for display flex for ændring af rækkefølge

---
+++ 3semestereksamen/src/components/FooterDesktop.vue
@@ -0,0 +1,43 @@
+<script setup>
+</script>
+<template>
+    <footer>
+        <div class="footercontent">
+            <h4>Åbningstider</h4>
+            <ul>
+                <li>Mandag - torsdag: kl. 11.00 - 17.30</li>
+                <li>Fredag: kl. 11.00 - 18.00</li>
+                <li>Sødag: Lukket</li>
+            </ul>
+            <h4>Konkaktoplysinger</h4>
+            <ul>
+                <li><font-awesome-icon icon="fa-solid fa-location-dot" class="fa-lg" /> Brandtspassage 30, 5000 Odense C</li>
+                <li><font-awesome-icon icon="fa-solid fa-envelope" class="fa-lg" /> info@dinavejling.dk</li>
+                <li><font-awesome-icon icon="fa-solid fa-phone" class="fa-lg"/> +45 28 30 14 05</li>
+            </ul>
+            <h4>Informationer</h4>
+            <ul>
+                <li>Om Dina Vejling</li>
+                <li>Handelsbetingelser</li>
+                <li>Galleri Rummet</li>
+                <li>Arrengementer</li>
+                <li>Kunstnere</li>
+            </ul>
+        
+            <h4>Følg Dina Vejling</h4>
+            <div class="some">
+                <font-awesome-icon icon="fa-brands fa-instagram" class="fa-4x"/>
+                <font-awesome-icon icon="fa-brands fa-square-facebook" class="fa-4x"/>
+            </div>
+            <img src="../assets/dinavejling.svg" alt="" class="logo">
+            
+        </div>
+        <div class="footerbanner">
+            <p>@ 2025 Dina Vejling - Dansk Kunsthåndværk</p>
+            <p>Drevet af Dina Vejling</p>
+        </div>
+    </footer>
+</template>
+
+<style>
+</style>
--- 3semestereksamen/src/views/HomeView.vue
+++ 3semestereksamen/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import FooterBottom from '../components/FooterBottom.vue';
 import Carrusel from '@/components/Carrusel.vue';
+import FooterDesktop from '@/components/FooterDesktop.vue';
 </script>
 
 <template>
   <Carrusel/>
   <FooterBottom/>
+  <FooterDesktop/>
 </template>
