d8359c764fda8071cab8a3ea4282241b1250894f
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-17 17:08:37 +0100

    tilpassning af fonts

--- 3semestereksamen/src/components/Arrangement.vue
+++ 3semestereksamen/src/components/Arrangement.vue
@@ -1,13 +1,24 @@
 <script setup>
 </script>
 <template>
-    <div class="arrangemant">
+    <div class="arrangement">
         <img src="../assets/julelys.webp" alt="">
         <p><b>14. November kl.17</b></p>
         <h2>Kom og tænd julelys i Hjertet af Odense</h2>
         <p>julelyset tændes i Passagen. Hygge, fællesang og madboder. Kom i god tid til nedtællingen</p>
     </div>
-
+    <div class="arrangement">
+        <img src="../assets/julelys.webp" alt="">
+        <p><b>5. December kl. 11-18</b></p>
+        <h2>Rikkes smukke smykkeunivers og Sejr CPHs brogede lædervarer </h2>
+        <p>Besøg af smykkekunsteren Rikke Lunneman & designeren Birgitte Sejersen</p>
+    </div>
+   <div class="arrangement">
+        <img src="../assets/julelys.webp" alt="">
+        <p><b>5. December kl. 11-18</b></p>
+        <h2>Rikkes smukke smykkeunivers og Sejr CPHs brogede lædervarer </h2>
+        <p>Besøg af smykkekunsteren Rikke Lunneman & designeren Birgitte Sejersen</p>
+    </div>
 </template>
 <style>
 </style>
--- 3semestereksamen/src/views/HomeView.vue
+++ 3semestereksamen/src/views/HomeView.vue
@@ -5,6 +5,27 @@ import FooterBottom from '../components/FooterBottom.vue';
 </script>
 
 <template>
+  <div class="aabningstider">
+    <h1>Åbningstider</h1>
+
+    <h4> Mandag - torsdag: kl. 11.00 - 17.30 <br>
+      Fredag: kl. 11.00 - 18.00 <br>
+      Lørdag: 10.30 - 14.00 <br>
+      Søndag: Lukket <br>
+    </h4>
+    <p>Dina Vejling - Dnask Kunsthåndværk</p>
+    <br>
+    <p>Brandts passage 30, 5000 Odense C <br>
+      Tlf. +45 28 30 14 05
+    </p>
+
+    <img src="../assets/dina_vejling_forside_1080x.webp" alt="">
+  </div>
+
+
+<div class="dividerimage">
+<img src="../assets/julelys.webp" alt="">
+</div>
   <Arrangement/>
   <Carrusel/>
   <FooterBottom/>
