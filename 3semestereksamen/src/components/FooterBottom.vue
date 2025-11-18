af80ecfb0ecec8e64a486238f8bd886c4fb21d82
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-18 10:34:44 +0100

    test virkede ikke

--- 3semestereksamen/src/components/FooterBottom.vue
+++ 3semestereksamen/src/components/FooterBottom.vue
@@ -3,45 +3,37 @@
 <template>
     <footer>
         <div class="footercontent">
-            <div class="footerright">
-                <div class="footerelements">
-                    <h4> 1 Åbningstider</h4>
-                    <ul>
-                        <li>Mandag - torsdag: kl. 11.00 - 17.30</li>
-                        <li>Fredag: kl. 11.00 - 18.00</li>
-                        <li>Sødag: Lukket</li>
-                    </ul>
-                </div>
+            <div class="footerelements">
+                <h4> 1 Åbningstider</h4>
+                <ul>
+                    <li>Mandag - torsdag: kl. 11.00 - 17.30</li>
+                    <li>Fredag: kl. 11.00 - 18.00</li>
+                    <li>Sødag: Lukket</li>
+                </ul>
             </div>
-            <div class="footerleft">
-                <div class="footerelements">
-                    <h4> 2 Konkaktoplysinger</h4>
-                    <ul>
-                        <li><font-awesome-icon icon="fa-solid fa-location-dot" class="fa-lg" /> Brandtspassage 30, 5000 Odense C</li>
-                        <li><font-awesome-icon icon="fa-solid fa-envelope" class="fa-lg" /> info@dinavejling.dk</li>
-                        <li><font-awesome-icon icon="fa-solid fa-phone" class="fa-lg"/> +45 28 30 14 05</li>
-                    </ul>
-                </div>
+            <div class="footerelements">
+                <h4> 2 Konkaktoplysinger</h4>
+                <ul>
+                    <li><font-awesome-icon icon="fa-solid fa-location-dot" class="fa-lg" /> Brandtspassage 30, 5000 Odense C</li>
+                    <li><font-awesome-icon icon="fa-solid fa-envelope" class="fa-lg" /> info@dinavejling.dk</li>
+                    <li><font-awesome-icon icon="fa-solid fa-phone" class="fa-lg"/> +45 28 30 14 05</li>
+                </ul>
             </div>
-            <div class="footerright">
-                <div class="footerelements">
-                    <h4> 3 Informationer</h4>
-                    <ul>
-                        <li>Om Dina Vejling</li>
-                        <li>Handelsbetingelser</li>
-                        <li>Galleri Rummet</li>
-                        <li>Arrengementer</li>
-                        <li>Kunstnere</li>
-                    </ul>
-                </div>
+            <div class="footerelements">
+                <h4> 3 Informationer</h4>
+                <ul>
+                    <li>Om Dina Vejling</li>
+                    <li>Handelsbetingelser</li>
+                    <li>Galleri Rummet</li>
+                    <li>Arrengementer</li>
+                    <li>Kunstnere</li>
+                </ul>
             </div>
-            <div class="footerleft">
-                <div class="footerelements">
-                    <h4> 4 Følg Dina Vejling</h4>
-                    <div class="some">
-                        <font-awesome-icon icon="fa-brands fa-instagram" class="fa-4x"/>
-                        <font-awesome-icon icon="fa-brands fa-square-facebook" class="fa-4x"/>
-                    </div>
+            <div class="footerelements">
+                <h4> 4 Følg Dina Vejling</h4>
+                <div class="some">
+                    <font-awesome-icon icon="fa-brands fa-instagram" class="fa-4x"/>
+                    <font-awesome-icon icon="fa-brands fa-square-facebook" class="fa-4x"/>
                 </div>
             </div>
             <div class="footerelements">
@@ -55,5 +47,6 @@
         </div>
     </footer>
 </template>
+
 <style>
 </style>
