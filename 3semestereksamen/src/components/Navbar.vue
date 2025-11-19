0b8e01753a293abf66c2a2a1552dd6e2734c82a8
SebastianSalling <sebastian-benjamin-s@hotmail.com>
2025-11-19 11:58:59 +0100

    Update navbar

--- 3semestereksamen/src/components/Navbar.vue
+++ 3semestereksamen/src/components/Navbar.vue
@@ -3,17 +3,17 @@
 
 <template>
     <nav>
-        <div>
+        <div id="menu-logo">
             <img src="../assets/dinavejling.svg" alt="Dina Vejling logo">
         </div>
-        <ul>
+        <ul id="menu-punkter">
             <li><a href="">Katalog</a></li>
             <li><a href="">Arrangementer</a></li>
             <li><a href="">Kunstnere</a></li>
             <li><a href="">Galleri RUMMET</a></li>
             <li><a href="">Om os</a></li>
         </ul>
-        <ul>
+        <ul id="menu-icons">
             <li><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></li>
             <li><a href=""><img src="" alt=""></a></li>
         </ul>
