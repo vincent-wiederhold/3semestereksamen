59107258070ef681542c8f35707c6d67a12798d1
SebastianSalling <sebastian-benjamin-s@hotmail.com>
2025-11-19 12:51:26 +0100

    Navbar update

--- 3semestereksamen/src/components/Navbar.vue
+++ 3semestereksamen/src/components/Navbar.vue
@@ -6,17 +6,19 @@
         <div id="menu-logo">
             <img src="../assets/dinavejling.svg" alt="Dina Vejling logo">
         </div>
-        <ul id="menu-punkter">
-            <li><a href="">Katalog</a></li>
-            <li><a href="">Arrangementer</a></li>
-            <li><a href="">Kunstnere</a></li>
-            <li><a href="">Galleri RUMMET</a></li>
-            <li><a href="">Om os</a></li>
-        </ul>
-        <ul id="menu-icons">
-            <li><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></li>
-            <li><a href=""><img src="" alt=""></a></li>
-        </ul>
+        <div id="menu-items">
+            <ul id="menu-punkter">
+                <li>Katalog <font-awesome-icon icon="fa-solid fa-angle-down" /></li>
+                <li><a href="">Arrangementer</a></li>
+                <li><a href="">Kunstnere</a></li>
+                <li><a href="">Galleri RUMMET</a></li>
+                <li><a href="">Om os</a></li>
+            </ul>
+            <ul id="menu-icons">
+                <li><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></li>
+                <li><a href=""><img src="" alt=""></a></li>
+            </ul>
+        </div>
     </nav>
     
 </template>
