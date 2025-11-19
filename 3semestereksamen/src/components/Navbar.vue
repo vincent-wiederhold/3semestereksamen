fc514067e13f6b0440882a122dd631777d7b59d3
SebastianSalling <sebastian-benjamin-s@hotmail.com>
2025-11-19 13:20:00 +0100

    Navbar update

--- 3semestereksamen/src/components/Navbar.vue
+++ 3semestereksamen/src/components/Navbar.vue
@@ -8,7 +8,9 @@
         </div>
         <div id="menu-items">
             <ul id="menu-punkter">
-                <li>Katalog <font-awesome-icon icon="fa-solid fa-angle-down" /></li>
+                <li>
+                    Katalog<font-awesome-icon icon="fa-solid fa-angle-down" />
+                </li>
                 <li><a href="">Arrangementer</a></li>
                 <li><a href="">Kunstnere</a></li>
                 <li><a href="">Galleri RUMMET</a></li>
@@ -16,7 +18,7 @@
             </ul>
             <ul id="menu-icons">
                 <li><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></li>
-                <li><a href=""><img src="" alt=""></a></li>
+                <li><font-awesome-icon icon="fa-solid fa-cart-shopping" /></li>
             </ul>
         </div>
     </nav>
