a93df4d329407e197d9b783a5af6372c7021743e
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-20 20:06:41 +0100

    Update Carrusel.vue

--- 3semestereksamen/src/components/Carrusel.vue
+++ 3semestereksamen/src/components/Carrusel.vue
@@ -3,6 +3,9 @@
 <template>
     <h1>Populære kategorier</h1>
     <p>Se større udvalg af alle kategorier i butikken</p>
+    <p><br><br><br>
+        Her hentes dataerne
+    </p>
 </template>
 <style>
 </style>
