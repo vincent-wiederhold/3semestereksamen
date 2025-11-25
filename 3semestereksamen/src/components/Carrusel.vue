fbbea55677138f250297470a89f0294e47dc44bf
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-25 12:05:36 +0100

    Update Carrusel.vue

--- 3semestereksamen/src/components/Carrusel.vue
+++ 3semestereksamen/src/components/Carrusel.vue
@@ -1,4 +1,17 @@
 <script setup>
+const getKunstner = () => {
+    fetch('https://semestereksamen-85cb6-default-rtdb.europe-west1.firebasedatabase.app/kunstner.json',{
+    method: 'GET'
+})
+    .then((response) => {
+        return response.json();
+    })
+    .then((res) => {
+        console.log(res);
+    })
+};
+
+getKunstner();
 </script>
 <template>
     <div class="carruselsection">
