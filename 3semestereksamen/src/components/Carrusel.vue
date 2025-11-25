482a858028d38d667975bf02d0d0517b35677e25
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-25 21:41:09 +0100

    Update Carrusel.vue

--- 3semestereksamen/src/components/Carrusel.vue
+++ 3semestereksamen/src/components/Carrusel.vue
@@ -2,6 +2,7 @@
 import {ref } from 'vue';
 
 const kunstner = ref([]);
+const startIndex = ref(0)
 
 const getKunstner = async () => {
     try{
@@ -29,17 +30,39 @@ const next = () => {
     if( startIndex.value >= kunstner.value.length) {
         startIndex.value = 0;
     }
-}
+};
+
+const getVisibleKunstnere = () => {
+    const result = [];
+    for (let i = 0; i< 3; i++){
+        let visibleIndex = startIndex.value + i;
+        if( visibleIndex >= kunstner.value.length) {
+            visibleIndex = visibleIndex - kunstner.value.length;
+        }
+        result.push(kunstner.value[visibleIndex]);
+    }
+    return result;
+};
+
+const prev = () => {
+    startIndex.value--;
+    if (startIndex.value< 0) {
+        startIndex.value = kunstner.value.length -1;
+    }
+};
+
 </script>
 <template>
     <div class="carruselsection">
         <h1>Populære Kunstnere</h1>
         <p>Kom ned i butikken og spørg ind til kunstnerne og deres værker eller læs mere ved at trykke på dem</p>
         <div class="kunstnere">
-            <li v-for="(kunstnerData, index) in kunstner" :key="index">
+            <button v-on:click="prev"><</button>
+            <li v-for="(kunstnerData, index) in getVisibleKunstnere(s)" :key="index">
                 <p>{{ kunstnerData.Kunstnernavn }}</p>
                 <p>{{ kunstnerData.Profession }}</p>
             </li>
+            <button v-on:click="next">></button>
         </div>
     </div>
 </template>
