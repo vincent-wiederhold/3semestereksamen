def05f5feb21279481f988a966847d0206db8897
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-26 14:48:01 +0100

    opdatering

--- 3semestereksamen/src/components/Arrangement.vue
+++ 3semestereksamen/src/components/Arrangement.vue
@@ -5,22 +5,28 @@
         <h1>Arrangementer</h1>
         <div class="row">
             <div class="individualarrangements">
-                <img src="../assets/julelys.webp" alt="">
+                <div class="arrangementbillede">
+                    <img src="../assets/julelys.webp" alt="">
+                </div>
                 <p><b>14. November kl.17</b></p>
                 <h2>Kom og tænd julelys i Hjertet af Odense</h2>
                 <p>julelyset tændes i Passagen. Hygge, fællesang og madboder. Kom i god tid til nedtællingen</p>
             </div>
             <div class="individualarrangements">
-                <img src="../assets/julelys.webp" alt="">
+                <div class="arrangementbillede">
+                    <img src="../assets/julelys.webp" alt="">
+                </div>
                 <p><b>5. December kl. 11-18</b></p>
                 <h2>Rikkes smukke smykkeunivers og Sejr CPHs brogede lædervarer </h2>
                 <p>Besøg af smykkekunsteren Rikke Lunneman & designeren Birgitte Sejersen</p>
             </div>
             <div class="individualarrangements">
-                <img src="../assets/julelys.webp" alt="">
-                <p><b>5. December kl. 11-18</b></p>
-                <h2>Rikkes smukke smykkeunivers og Sejr CPHs brogede lædervarer </h2>
-                <p>Besøg af smykkekunsteren Rikke Lunneman & designeren Birgitte Sejersen</p>
+                <div class="arrangementbillede">
+                    <img src="../assets/Sussie_krull.webp" alt="">
+                </div>
+                <p><b>15. Januar kl. 17</b></p>
+                <h2>Kom og se de bæredygtige vaser</h2>
+                <p>Sussi Krull viser, hvordan hendes bæredygtige vaser bliver lavet</p>
             </div>
         </div>
     </div>
