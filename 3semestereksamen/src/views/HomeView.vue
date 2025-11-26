4c2c20e710643f997e5e83a2cfbb44ebe043a080
SebastianSalling <sebastian-benjamin-s@hotmail.com>
2025-11-26 11:46:46 +0100

    Tilføjet logo til hero video

--- 3semestereksamen/src/views/HomeView.vue
+++ 3semestereksamen/src/views/HomeView.vue
@@ -45,6 +45,10 @@ const handlePause = () => (isPlaying.value = false);
     <source src="../assets/dinahero.mp4" />
     </video>
 
+    <div class="hero-logo">
+      <img src="../assets/dinavejling.svg" alt="Dina Vejling logo">
+    </div>
+
     <div class="controls">
       <button v-on:click="togglePlay">
         <font-awesome-icon v-if="isPlaying" icon="fa-solid fa-pause" />
@@ -52,6 +56,8 @@ const handlePause = () => (isPlaying.value = false);
       </button>
     </div>
   </figure>
+
+
   <div class="landingpagesection">
     <h1>DANSK KUNSTHÅNDVÆRK MED SJÆL</h1>
     <p>Hos Dina Vejling kan du gå på opdagelse i en verden af håndplukkede værker fra over 70 professionelle kunstnere. Find os i Brandts passage i Odense.</p>
