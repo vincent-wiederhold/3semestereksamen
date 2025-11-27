885e08fc5341b8bdd983e4df59e99f1c437fd030
Vincent Wiederhold <vincent.taliesin@gmail.com>
2025-11-27 13:41:30 +0100

    kategori side

--- 3semestereksamen/src/views/CategoriesView.vue
+++ 3semestereksamen/src/views/CategoriesView.vue
@@ -11,17 +11,45 @@ import FooterBottom from '@/components/FooterBottom.vue';
 <div class="section-wrapper">
     <h2>Se udvalget af Krus og kopperne herunder</h2>
     <p>De keramiske krus og kopper er hos os speciallavet kunsthåndværk, flere forskellige slags kan ses an i butikken</p>
-
-    <div class="kategoriprodukt">
+<div class="kategoriprodukter">
+    <div class="kategoriproduktright">
         <div class="kategoriprodukt-beskrivelse">
-            <h2>Farvet krus</h2>
-            <p><b>Af CLip Clap</b></p>
-            <p>275 kr</p>
+            <div class="tekst">
+                <h2>Farvet krus</h2>
+                <p><b>Af CLip Clap</b></p>
+                <p>275 kr</p>
+            </div>
         </div>
         <div class="kategoriprodukt-billede">
             <img src="../assets\images\produkter\kopper\krus-stor-lyseblå-claire-maria-lehmann-og-iben-harboe.webp" alt="">
         </div>
     </div>
+    <div class="kategoriproduktleft">
+        <div class="kategoriprodukt-beskrivelse">
+            <div class="tekst">
+                <h2>Farvet krus</h2>
+                <p><b>Af CLip Clap</b></p>
+                <p>275 kr</p>
+            </div>
+        </div>
+        <div class="kategoriprodukt-billede">
+            <img src="../assets\images\produkter\kopper\krus-stor-lyseblå-claire-maria-lehmann-og-iben-harboe.webp" alt="">
+        </div>
+    </div>
+    <div class="kategoriproduktright">
+        <div class="kategoriprodukt-beskrivelse">
+            <div class="tekst">
+                <h2>Farvet krus</h2>
+                <p><b>Af CLip Clap</b></p>
+                <p>275 kr</p>
+            </div>
+        </div>
+        <div class="kategoriprodukt-billede">
+            <img src="../assets\images\produkter\kopper\krus-stor-lyseblå-claire-maria-lehmann-og-iben-harboe.webp" alt="">
+        </div>
+    </div>
+</div>
+
 </div>
     <FooterBottom/>
 </template>
